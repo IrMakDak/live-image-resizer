a1a6e97052637fcada7b717334c6c2e8590db373
Irina Pruglo <ipruglo@hu-li.com>
2025-01-26 00:39:06 +0100

    tracking deletions & ability to delete photos via telegram bot & add sqlite

--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -1,6 +1,14 @@
+import os
 import aiohttp
+import base64
+import hashlib
+import time
 
-from telegram import Update
+from telegram import (
+    Update,
+)
+from dotenv import load_dotenv
+from functools import wraps
 from telegram.ext import (
     Application,
     CommandHandler,
@@ -10,65 +18,130 @@ from telegram.ext import (
 )
 from pathlib import Path
 
-SERVER_PORT = 5000
-PROCESS_IMAGE_HOST = "http://127.0.0.1"
-PROCESS_IMAGE_ROUTE = "/process_image"
-RANDOM_IMAGE_ROUTE = "/random_image"
-# TELEGRAM_TOKEN = "мой_токен"
-UPLOAD_DIR = "../originals"  # Папка для хранения оригиналов
+load_dotenv()
+
+
+SERVER_PORT = int(os.getenv("SERVER_PORT", 5001))
+SERVER_HOST = os.getenv("SERVER_HOST", "http://127.0.0.1")
+
+
+SERVER_PATH = f"{SERVER_HOST}:{SERVER_PORT}"
+
+RANDOM_IMAGE_ROUTE = "random-image"
+
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+
+UPLOAD_DIR = os.getenv("ORIGINALS_PATH", "../originals")
+
 
 Path(UPLOAD_DIR).mkdir(exist_ok=True)
 
 
+def handle_error():
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(
+            update: Update, context: ContextTypes.DEFAULT_TYPE, *args, **kwargs
+        ):
+            try:
+                return await func(update, context, *args, **kwargs)
+            except aiohttp.ClientError as e:
+                if update.message is not None:
+                    await update.message.reply_text(
+                        f"❌ Ошибка соединения с сервером: {str(e)}"
+                    )
+                elif update.callback_query is not None:
+                    await update.callback_query.answer(
+                        f"❌ Ошибка соединения с сервером"
+                    )
+            except Exception as e:
+                if update.message is not None:
+                    await update.message.reply_text(f"❌ Ошибка: {str(e)}")
+
+        return wrapper
+
+    return decorator
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "Привет! Отправь мне фотографию, и я сохраню её в папку resized с новым размером.\n"
-        "Используй /random чтобы получить случайное обработанное изображение."
-    )
+    if update.message is not None:
+        await update.message.reply_text(
+            "Отправь мне фотку и я буду показывать ее в рамочке.\n"
+            "/random - получить случайную фотку\n",
+            "/delete <id> - удалить фото\n",
+        )
 
 
-async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    photo = update.message.photo[-1]  # Берём самое большое разрешение
+# TODO: опираясь на статус отдавать id после обработки?
+@handle_error()
+async def handle_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message is not None:
+        image = update.message.photo[-1]
 
-    try:
         # Скачиваем фото
-        file = await context.bot.get_file(photo.file_id)
-        file_path = Path(UPLOAD_DIR) / f"{photo.file_id}.jpg"
+        file = await context.bot.get_file(image.file_id)
+        file_path = Path(UPLOAD_DIR) / f"{image.file_id}.jpg"
+        if file_path.exists():
+            await update.message.reply_text("❌ Такое фото уже есть!")
+            return
 
         await file.download_to_drive(file_path)
+        if update.message is None:
+            raise
 
-        await update.message.reply_text(f"✅ Фото загружено!\n{photo.file_id}")
-
-    except aiohttp.ClientError as e:
-        await update.message.reply_text(f"❌ Ошибка соединения с сервером: {str(e)}")
-    except Exception as e:
-        await update.message.reply_text(f"❌ Ошибка: {str(e)}")
+        await update.message.reply_text(f"✅ Получил!")
 
 
+@handle_error()
 async def get_random_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # Получить и отправить случайное изображение
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                f"{PROCESS_IMAGE_HOST}:{SERVER_PORT}{RANDOM_IMAGE_ROUTE}",
-            ) as response:
-                if response.status == 200:
-                    # Если получили изображение, отправляем его
-                    image_data = await response.read()
-                    await update.message.reply_photo(
-                        photo=image_data, caption="🎲 Случайное изображение из галереи"
-                    )
-                else:
-                    # Если получили ошибку, парсим JSON с ошибкой
-                    error_data = await response.json()
-                    await update.message.reply_text(
-                        f"❌ Не удалось получить изображение: {error_data.get('message', 'Неизвестная ошибка')}"
-                    )
 
-    except aiohttp.ClientError as e:
-        await update.message.reply_text(f"❌ Ошибка соединения с сервером: {str(e)}")
-    except Exception as e:
-        await update.message.reply_text(f"❌ Ошибка: {str(e)}")
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            f"{SERVER_PATH}/{RANDOM_IMAGE_ROUTE}",
+        ) as response:
+            if response.status == 200 and update.message is not None:
+                response_json = await response.json()
+                image_base64 = response_json.get("image")
+                image_data = base64.b64decode(image_base64)
+                file_hash = str(response_json.get("file_hash"))
+
+                await update.message.reply_photo(photo=image_data)
+                await update.message.reply_text(
+                    "🎲 Вот случайная фотка, её id...\nНапиши /delete <id> чтобы удалить её",
+                )
+                await update.message.reply_text(
+                    file_hash,
+                )
+
+            elif update.message is not None:
+                error_data = await response.json()
+                await update.message.reply_text(
+                    f"❌ Не удалось получить изображение: {error_data.get('message', 'Неизвестная ошибка')}"
+                )
+
+
+async def delete_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message is not None:
+        async with aiohttp.ClientSession() as session:
+
+            # context.args содержит список аргументов после команды
+            if not context.args:
+                await update.message.reply_text(
+                    f"❌ Вставь id фотки которую удалить хочешь"
+                )
+                return
+
+            file_hash = context.args[0]
+
+            async with session.delete(
+                f"{SERVER_PATH}/images/{file_hash}",
+            ) as response:
+                if response.status in (200, 204):
+                    await update.message.reply_text(text="✅ Фотка удалена!")
+                else:
+                    await update.message.reply_text(text="❌ Что-то пошло не так")
 
 
 def run_bot():
@@ -76,7 +149,8 @@ def run_bot():
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("random", get_random_image))
-    application.add_handler(MessageHandler(filters.PHOTO, handle_photo))
+    application.add_handler(CommandHandler("delete", delete_image))
+    application.add_handler(MessageHandler(filters.PHOTO, handle_image))
 
     application.run_polling()
 
--- watcher/main.py
+++ watcher/main.py
@@ -1,14 +1,19 @@
+import os
 import requests
 import time
-import argparse
 
 from watchdog.observers import Observer
 from pathlib import Path
 from watchdog.events import FileSystemEventHandler
+from dotenv import load_dotenv
 
-SERVER_PORT = 5000
-PROCESS_IMAGE_HOST = "http://127.0.0.1"
-PROCESS_IMAGE_ROUTE = "/process_image"
+load_dotenv()
+
+SERVER_PORT = int(os.getenv("SERVER_PORT", 5001))
+SERVER_HOST = os.getenv("SERVER_HOST")
+
+SERVER_PATH = f"{SERVER_HOST}:{SERVER_PORT}"
+
 
 SUPPORTED_EXTENSIONS = {
     ".jpg",
@@ -18,24 +23,124 @@ SUPPORTED_EXTENSIONS = {
 }
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(
-        description="Изменение размера изображений в папке"
-    )
-    parser.add_argument("directory_path", help="Путь к папке с изображениями")
-    parser.add_argument(
-        "--output",
-        "-o",
-        default="../resized",
-        help="Папка для сохранения результатов (по умолчанию: ../resized)",
-    )
-    return parser.parse_args()
+def request_processing(image_path):
+    """Отправляет изображение на обработку серверу"""
+    try:
+        absolute_path = Path(image_path).resolve()
+
+        response = requests.post(
+            f"{SERVER_PATH}/images",
+            json={"file_path": str(absolute_path)},
+        )
+        response.raise_for_status()
+        print(f"Сервер ответил: {response.json()}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при отправке запроса: {str(e)}")
+    except Exception as e:
+        print(f"Неожиданная ошибка при обработке {image_path.name}: {str(e)}")
+
+
+def request_deletion(image_path: Path):
+    """Отправляет изображение на обработку серверу"""
+    try:
+        absolute_path = Path(image_path).resolve()
+        response = requests.get(
+            f"{SERVER_PATH}/images/get-image-id",
+            params={"file_path": str(absolute_path)},
+        )
+        response.raise_for_status()
+
+        response_data = response.json()
+        if not response_data or "file_hash" not in response_data:
+            print(f"Ошибка: Не получен hash файла от сервера. Ответ: {response_data}")
+            return
+
+        file_hash = response_data["file_hash"]
+        if not file_hash:  # Проверка на пустое значение
+            print("Ошибка: Получен пустой hash файла")
+            return
+
+        print(f"Получил hash фотки: {file_hash}. Буду удалять")
+
+        response = requests.delete(
+            f"{SERVER_PATH}/images/{file_hash}",
+        )
+        response.raise_for_status()
+        print("Удалил!")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при отправке запроса: {str(e)}")
+    except Exception as e:
+        print(f"Неожиданная ошибка при обработке {image_path.name}: {str(e)}")
+
+
+def remove_extra_processed_files(input_dir, output_folder):
+    """Проверяем лишние файлы в output папке"""
+    input_path = Path(input_dir)
+    output_path = Path(output_folder)
+
+    output_files = [
+        f
+        for f in output_path.glob("*")
+        if f.suffix.lower() in SUPPORTED_EXTENSIONS and f.is_file()
+    ]
+
+    extra_files = []
+    for output_file in output_files:
+        input_file = input_path / output_file.name
+        if not input_file.exists():
+            extra_files.append(output_file)
+
+    if extra_files:
+        print(f"\nЯ нашел {len(extra_files)} лишних файлов в output папке:")
+        for file_path in extra_files:
+            print(f"Удалю: {file_path.name}")
+            file_path.unlink()
+    return {"to_remove": len(extra_files)}
+
+
+def check_missing_images(input_dir, output_folder):
+    """Проверяет наличие всех изображений из input в output папке"""
+    input_path = Path(input_dir)
+    output_path = Path(output_folder)
+
+    # Получаем списки файлов из обеих папок
+    input_files = [
+        f
+        for f in input_path.glob("*")
+        if f.suffix.lower() in SUPPORTED_EXTENSIONS and f.is_file()
+    ]
+
+    if not input_files:
+        print("Входная папка пуста")
+        return
+
+    # Проверяем отсутствующие файлы
+    unprocessed_files = []
+    for input_file in input_files:
+        output_file = output_path / input_file.name
+        if not output_file.exists():
+            unprocessed_files.append(input_file)
+
+    # Обрабатываем ненайденные файлы
+    if unprocessed_files:
+        print(f"Я нашел {len(unprocessed_files)} необработанных изображений :(")
+        for file_path in unprocessed_files:
+            print(f"Пробую обработать {file_path.name}")
+            request_processing(file_path)
+    else:
+        print("Все изображения уже обработаны ;)")
+
+    return {
+        "to_process": len(unprocessed_files),
+        **remove_extra_processed_files(input_dir, output_folder),
+    }
 
 
 class ImageHandler(FileSystemEventHandler):
     def __init__(self, output_path):
         self.output_path = Path(output_path)
-        self.output_path.mkdir(exist_ok=True)
 
     def on_created(self, event):
         # нам нужно обрабатывать только файлы изображений, а не папки
@@ -44,31 +149,40 @@ class ImageHandler(FileSystemEventHandler):
 
         file_path = Path(event.src_path)
         if file_path.suffix.lower() in SUPPORTED_EXTENSIONS:
-            try:
-                response = requests.post(
-                    f"{PROCESS_IMAGE_HOST}:{SERVER_PORT}{PROCESS_IMAGE_ROUTE}",
-                    json={"file_path": str(event.src_path)},
+            output_file = self.output_path / file_path.name
+            if output_file.exists():
+                print(
+                    f"File {file_path.name} already exists in output folder, skipping"
                 )
-                # Проверяем статус-код
-                response.raise_for_status()
+                return
+            file_path = Path(event.src_path)
+            request_processing(file_path)
 
-                print(f"Сервер ответил: {response.json()}")
-            except requests.exceptions.RequestException as e:
-                print(f"Ошибка при отправке запроса: {str(e)}")
-            except Exception as e:
-                print(f"Неожиданная ошибка при обработке {file_path.name}: {str(e)}")
+    def on_deleted(self, event):
+        if event.is_directory:
+            return
+
+        file_path = Path(event.src_path)
+        if file_path.suffix.lower() in SUPPORTED_EXTENSIONS:
+
+            output_file = self.output_path / file_path.name
+            if output_file.exists():
+                print(
+                    f"Я видел как ты удалил {file_path.name} из input папки. Сейчас удалю и из output папки."
+                )
+                request_deletion(file_path)
+                output_file.unlink()
 
 
-def watch_directory(directory_path, output_dir):
-    event_handler = ImageHandler(output_dir)
+def watch_directory(input_folder, output_folder):
+    event_handler = ImageHandler(output_folder)
     observer = Observer()
-    observer.schedule(event_handler, directory_path, recursive=False)
+    observer.schedule(event_handler, input_folder, recursive=False)
     observer.start()
 
     try:
-        print(f"Начато отслеживание папки: {directory_path}")
-        print(f"Изображения будут сохраняться в: {output_dir}")
-        print("Нажмите Ctrl+C для завершения...")
+        print(f"Начато отслеживание папки: {input_folder}")
+        print(f"Изображения будут сохраняться в: {output_folder}")
         while True:
             time.sleep(1)
 
@@ -81,5 +195,11 @@ def watch_directory(directory_path, output_dir):
 
 
 if __name__ == "__main__":
-    args = parse_args()
-    watch_directory(args.directory_path, args.output)
+    input_folder = "../input_photos"
+    output_folder = "../processed_photos"
+
+    os.makedirs(input_folder, exist_ok=True)
+    os.makedirs(output_folder, exist_ok=True)
+
+    check_missing_images(input_folder, output_folder)
+    watch_directory(input_folder, output_folder)
